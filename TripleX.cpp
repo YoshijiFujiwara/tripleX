2d1d797f9f23100ad941c1a08b8a92841779069e
YoshijiFujiwara <yfbiology2@gmail.com>
2020-08-18 01:37:13 +0900

    adding difficulties

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
-  std::cout << "You are a secret agent breaking into a secure server room...\n";
-  std::cout << "Enter the correct code to continue...\n";
+  std::cout << "You are a secret agent breaking into a " << Difficulty;
+  std::cout << " secure server room...\nEnter the correct code to continue...\n";
+}
 
-  const int CodeA = 4;
-  const int CodeB = 3;
-  const int CodeC = 2;
+bool PlayGame(int Difficulty)
+{
+  PrintIntroduction(Difficulty);
+  const int CodeA = rand() % Difficulty + 1;
+  const int CodeB = rand() % Difficulty + 1;
+  const int CodeC = rand() % Difficulty + 1;
 
   const int CodeSum = CodeA + CodeB + CodeC;
   const int CodeProduct = CodeA * CodeB * CodeC;
@@ -28,11 +32,32 @@ int main()
   if (GuessSum == CodeSum && GuessProduct == CodeProduct)
   {
     std::cout << "\nYou win!";
+    return true;
   }
   else
   {
     std::cout << "\nYou lose!";
+    return false;
+  }
+}
+
+int main()
+{
+  int LevelDifficulty = 1;
+  int const MaxDifficulty = 5;
+
+  while(LevelDifficulty <= MaxDifficulty)
+  {
+    bool bLevelComplete = PlayGame(LevelDifficulty);
+    std::cin.clear(); // Clears any errors
+    std::cin.ignore(); // Discards the buffer
+
+    if (bLevelComplete)
+    {
+      ++LevelDifficulty;
+    }
   }
 
+  std::cout << "\n*** Great work again! You got all the files! Now get out of there! ***\n";
   return 0;
 }
